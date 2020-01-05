9da97e8a4eda7b247fb16035bbc0cb7f7fd34541
master-kot <master-kot@mail.ru>
2020-01-05 18:54:05 +0300

    homework for lesson 2 changed

---
+++ lesson2/src/main/java/CreatorDB.java
@@ -0,0 +1,94 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class CreatorDB {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        Class.forName("org.sqlite.JDBC");
+        Connection conn = DriverManager.getConnection("jdbc:sqlite:homework.db");
+        Statement stmt = conn.createStatement();
+
+        DBUtility dbUtility = new DBUtility();
+        //CreatorDB.createListOfEquipment(stmt);
+        //dbUtility.createPrinterTable(conn, stmt);
+        //dbUtility.AddPrinters(stmt);
+        //System.out.println(dbUtility.selectExpensivePC(stmt));
+        //System.out.println(dbUtility.selectQuickLaptop(stmt));
+        //dbUtility.AddPrinters(stmt, new Printer(1111, "Kyosera", "blue", "laser", 5555));
+        //System.out.println(dbUtility.selectMaker(stmt));
+        //dbUtility.makerWithMaxProceeds(stmt);
+        //CreatorDB.createCommonBase(stmt);
+    }
+    public static void createListOfEquipment (Statement stmt) {
+        try {
+            stmt.execute("CREATE TABLE IF NOT EXISTS Product (maker TEXT, model INTEGER, type TEXT);");
+            stmt.execute("CREATE TABLE IF NOT EXISTS Laptop (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, model INTEGER, speed INTEGER, ram INTEGER, hd INTEGER, screen INTEGER, price INTEGER);");
+            stmt.execute("CREATE TABLE IF NOT EXISTS PC (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, model INTEGER, speed INTEGER, ram INTEGER, hd INTEGER, price INTEGER);");
+            stmt.execute("INSERT INTO Laptop(model, speed, ram, hd, screen, price)  VALUES(1360, 3600, 16, 1000, 19, 70000);");
+            stmt.execute("INSERT INTO Product(maker, model, type) VALUES('Intel', 1360, 'Laptop');");
+            stmt.execute("INSERT INTO PC(model, speed, ram, hd, price)  VALUES(2210, 3600, 8, 1000, 35000);");
+            stmt.execute("INSERT INTO Product(maker, model, type) VALUES('Intel', 2210, 'PC');");
+            stmt.execute("INSERT INTO PC(model, speed, ram, hd, price)  VALUES(2205, 2200, 4, 1000, 22000);");
+            stmt.execute("INSERT INTO Product(maker, model, type) VALUES('AMD', 2205, 'PC');");
+            stmt.execute("INSERT INTO PC(model, speed, ram, hd, price)  VALUES(2205, 2200, 4, 1000, 22000);");
+            stmt.execute("INSERT INTO Product(maker, model, type) VALUES('AMD', 2205, 'PC');");
+            stmt.execute("INSERT INTO Laptop(model, speed, ram, hd, screen, price)  VALUES(1330, 3000, 8, 1000, 17, 35000);");
+            stmt.execute("INSERT INTO Product(maker, model, type) VALUES('Dell', 1330, 'Laptop');");
+            stmt.execute("INSERT INTO Laptop(model, speed, ram, hd, screen, price)  VALUES(1305, 2000, 4, 500, 15, 15000);");
+            stmt.execute("INSERT INTO Product(maker, model, type) VALUES('HP', 1305, 'Laptop');");
+            stmt.execute("INSERT INTO Laptop(model, speed, ram, hd, screen, price)  VALUES(1305, 2000, 4, 500, 15, 15000);");
+            stmt.execute("INSERT INTO Product(maker, model, type) VALUES('HP', 1305, 'Laptop');");
+            stmt.execute("INSERT INTO Laptop(model, speed, ram, hd, screen, price)  VALUES(1305, 2000, 4, 500, 15, 15000);");
+            stmt.execute("INSERT INTO Product(maker, model, type) VALUES('HP', 1305, 'Laptop');");
+            stmt.execute("INSERT INTO PC(model, speed, ram, hd, price)  VALUES(2210, 3600, 8, 1000, 35000);");
+            stmt.execute("INSERT INTO Product(maker, model, type) VALUES('Intel', 2210, 'PC');");
+            stmt.execute("INSERT INTO Laptop(model, speed, ram, hd, screen, price)  VALUES(1305, 2000, 4, 500, 15, 15000);");
+            stmt.execute("INSERT INTO Product(maker, model, type) VALUES('HP', 1305, 'Laptop');");
+            stmt.execute("INSERT INTO Laptop(model, speed, ram, hd, screen, price)  VALUES(1305, 2000, 4, 500, 15, 5000);");
+            stmt.execute("INSERT INTO Product(maker, model, type) VALUES('HP', 1305, 'Laptop');");
+            stmt.execute("INSERT INTO Laptop(model, speed, ram, hd, screen, price)  VALUES(1290, 1200, 2, 500, 14, 10000);");
+            stmt.execute("INSERT INTO Product(maker, model, type) VALUES('Acer', 1290, 'Laptop');");
+            stmt.execute("INSERT INTO Laptop(model, speed, ram, hd, screen, price)  VALUES(1290, 1200, 2, 500, 14, 10000);");
+            stmt.execute("INSERT INTO Product(maker, model, type) VALUES('Acer', 1290, 'Laptop');");
+            stmt.execute("INSERT INTO Laptop(model, speed, ram, hd, screen, price)  VALUES(1290, 1200, 2, 500, 14, 10000);");
+            stmt.execute("INSERT INTO Product(maker, model, type) VALUES('Acer', 1290, 'Laptop');");
+            stmt.execute("INSERT INTO PC(model, speed, ram, hd, price)  VALUES(2200, 1700, 2, 500, 12000);");
+            stmt.execute("INSERT INTO PC(model, speed, ram, hd, price)  VALUES(2200, 1700, 2, 500, 12000);");
+            stmt.execute("INSERT INTO PC(model, speed, ram, hd, price)  VALUES(2200, 1700, 2, 500, 12000);");
+            stmt.execute("INSERT INTO PC(model, speed, ram, hd, price)  VALUES(2200, 1700, 2, 500, 12000);");
+            stmt.execute("INSERT INTO Product(maker, model, type) VALUES('NEC', 2200, 'PC');");
+            stmt.execute("INSERT INTO Product(maker, model, type) VALUES('NEC', 2200, 'PC');");
+            stmt.execute("INSERT INTO Product(maker, model, type) VALUES('NEC', 2200, 'PC');");
+            stmt.execute("INSERT INTO Product(maker, model, type) VALUES('NEC', 2200, 'PC');");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void createCommonBase (Statement stmt){
+        try {
+            stmt.execute("CREATE TABLE IF NOT EXISTS Common(id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, maker TEXT, model INTEGER, type TEXT, speed INTEGER, ram INTEGER, hd INTEGER, screen INTEGER, price INTEGER);");
+            stmt.execute("INSERT INTO Common(maker, model, type, speed, ram, hd, screen, price) VALUES('Intel', 1360, 'Laptop', 3600, 16, 1000, 19, 70000);");
+            stmt.execute("INSERT INTO Common(maker, model, type, speed, ram, hd, screen, price) VALUES('Intel', 2210, 'PC', 3600, 8, 1000, 20, 35000);");
+            stmt.execute("INSERT INTO Common(maker, model, type, speed, ram, hd, screen, price) VALUES('AMD', 2205, 'PC', 2200, 4, 1000, 20, 22000);");
+            stmt.execute("INSERT INTO Common(maker, model, type, speed, ram, hd, screen, price) VALUES('AMD', 2205, 'PC', 2200, 4, 1000, 20, 22000);");
+            stmt.execute("INSERT INTO Common(maker, model, type, speed, ram, hd, screen, price) VALUES('Dell', 1330, 'Laptop', 3000, 8, 1000, 17, 35000);");
+            stmt.execute("INSERT INTO Common(maker, model, type, speed, ram, hd, screen, price) VALUES('HP', 1305, 'Laptop', 2000, 4, 500, 15, 15000);");
+            stmt.execute("INSERT INTO Common(maker, model, type, speed, ram, hd, screen, price) VALUES('HP', 1305, 'Laptop', 2000, 4, 500, 15, 15000);");
+            stmt.execute("INSERT INTO Common(maker, model, type, speed, ram, hd, screen, price) VALUES('HP', 1305, 'Laptop', 2000, 4, 500, 15, 15000);");
+            stmt.execute("INSERT INTO Common(maker, model, type, speed, ram, hd, screen, price) VALUES('Intel', 2210, 'PC', 3600, 8, 1000, 20, 35000);");
+            stmt.execute("INSERT INTO Common(maker, model, type, speed, ram, hd, screen, price) VALUES('HP', 1305, 'Laptop', 2000, 4, 500, 15, 150000);");
+            stmt.execute("INSERT INTO Common(maker, model, type, speed, ram, hd, screen, price) VALUES('HP', 1305, 'Laptop', 2000, 4, 500, 15, 5000);");
+            stmt.execute("INSERT INTO Common(maker, model, type, speed, ram, hd, screen, price) VALUES('Acer', 1290, 'Laptop', 1200, 2, 500, 14, 10000);");
+            stmt.execute("INSERT INTO Common(maker, model, type, speed, ram, hd, screen, price) VALUES('Acer', 1290, 'Laptop', 1200, 2, 500, 14, 10000);");
+            stmt.execute("INSERT INTO Common(maker, model, type, speed, ram, hd, screen, price) VALUES('Acer', 1290, 'Laptop', 1200, 2, 500, 14, 10000);");
+            stmt.execute("INSERT INTO Common(maker, model, type, speed, ram, hd, screen, price) VALUES('NEC', 2200, 'PC', 1700, 2, 500, 20, 12000);");
+            stmt.execute("INSERT INTO Common(maker, model, type, speed, ram, hd, screen, price) VALUES('NEC', 2200, 'PC', 1700, 2, 500, 20, 12000);");
+            stmt.execute("INSERT INTO Common(maker, model, type, speed, ram, hd, screen, price) VALUES('NEC', 2200, 'PC', 1700, 2, 500, 20, 12000);");
+            stmt.execute("INSERT INTO Common(maker, model, type, speed, ram, hd, screen, price) VALUES('NEC', 2200, 'PC', 1700, 2, 500, 20, 12000);");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
--- lesson2/src/main/java/DBUtility.java
+++ lesson2/src/main/java/DBUtility.java
@@ -1,21 +1,18 @@
-
-
 import java.sql.*;
 import java.util.ArrayList;
 
 public class DBUtility {
-    /*
-    Каждый из тасков решается одним SQL запросом
-    Создать таблицу принтеры, Printer(id INTEGER AI PK U, model INTEGER, color TEXT, type TEXT, price INTEGER)
-    добавить в нее 3 записи:
-    1 1012 col laser 20000 (производитель HP)
-    2 1010 bw jet 5000 (производитель Canon)
-    3 1010 bw jet 5000 (производитель Canon)
-    Каждая вставка в таблицу принтер должна отражаться добавлением записи в таблицу продукт
-    */
 
+    // TODO: 16.12.2019
+    /**
+     * Создать таблицу принтеры, Printer(id INTEGER AI PK U, model INTEGER, color TEXT, type TEXT, price INTEGER)
+     * добавить в нее 3 записи:
+     * 1 1012 col laser 20000 (производитель HP)
+     * 2 1010 bw jet 5000 (производитель Canon)
+     * 3 1010 bw jet 5000 (производитель Canon)
+     * Каждая вставка в таблицу принтер должна отражаться добавлением записи в таблицу продукт
+    */
     void AddPrinters(Statement stmt) {
-        // TODO: 16.12.2019
         try {
             stmt.execute(String.format("INSERT INTO Printer (model, color, type, price) VALUES (%d, %s, %s, %d);", 1012, "'col'", "'laser'", 20000));
             stmt.execute(String.format("INSERT INTO Product (maker, model, type) VALUES (%s, %d, %s);", "'HP'", 1012, "'Printer'"));
@@ -28,8 +25,11 @@ public class DBUtility {
         }
     }
 
+    // TODO: 16.12.2019
+    /**
+     * Аналогичный предыдущему метод, работающий с объектом класса Printer, добавляющий его значения в базу данных
+     */
     void AddPrinters(Statement stmt, Printer printer) {
-        // TODO: 16.12.2019
         try {
             stmt.execute(String.format("INSERT INTO Printer (model, color, type, price) VALUES (%d, '%s', '%s', %d);", printer.getModel(), printer.getColor(), printer.getType(), printer.getPrice()));
             stmt.execute(String.format("INSERT INTO Product (maker, model, type) VALUES ('%s', %d, '%s');", printer.getMaker(), printer.getModel(), printer.getType()));
@@ -38,8 +38,11 @@ public class DBUtility {
         }
     }
 
+    // TODO: 16.12.2019
+    /**
+     * Создает таблицу продуктов и принтеров если они не были созданы
+     */
     public void createPrinterTable(Connection con, Statement  stmt) {
-        // TODO: 16.12.2019
         try {
             stmt.execute("CREATE TABLE IF NOT EXISTS Printer(id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE, model INTEGER, color TEXT, type TEXT, price INTEGER);");
             stmt.execute("CREATE TABLE IF NOT EXISTS Product (maker TEXT, model INTEGER, type TEXT);");
@@ -48,10 +51,11 @@ public class DBUtility {
         }
     }
 
-    // Метод должен вернуть список уникальных моделей PC дороже 15 тысяч
-    // Метод возвращает: [2210, 2205]
+    // TODO: 16.12.2019
+    /**
+     * Метод должен вернуть список уникальных моделей PC дороже 15 тысяч
+     */
     public ArrayList<String> selectExpensivePC(Statement stmt) {
-        //todo
         ArrayList<String> result = new ArrayList<>();
         ResultSet rs = null;
         try {
@@ -66,10 +70,11 @@ public class DBUtility {
         return result;
     }
 
-    // Метод должен вернуть список id ноутов, скорость процессора которых выше чем 2500
-    // [1, 2]
+    // TODO: 16.12.2019
+    /**
+     * Метод должен вернуть список id ноутов, скорость процессора которых выше чем 2500
+     */
     public ArrayList<Integer> selectQuickLaptop(Statement stmt){
-        // TODO: 16.12.2019
         ArrayList<Integer> result = new ArrayList<>();
         ResultSet rs = null;
         try {
@@ -83,10 +88,11 @@ public class DBUtility {
         return result;
     }
 
-    // Метод должен вернуть список производителей которые делают и пк и ноутбуки
-    // Метод возвращает: [Intel]
+    // TODO: 16.12.2019
+    /**
+     * Метод должен вернуть список производителей которые делают и пк и ноутбуки
+     */
     public ArrayList<String> selectMaker(Statement stmt){
-        //todo
         ArrayList<String> result = new ArrayList<>();
         ArrayList<String> pcMakers = new ArrayList<>();
         ResultSet rs = null;
@@ -106,18 +112,15 @@ public class DBUtility {
         return result;
     }
 
-    /*
+    // TODO: 16.12.2019
+    /**
      * Метод должен вернуть максимальную среди всех произодителей суммарную стоимость всех изделий типов ноутбук или компьютер,
      * произведенных одним производителем. Необходимо объединить таблицы продуктов ноутбуков и компьютеров
      * и отгрупировать по сумме прайсов после чего выбрать максимум или сделать любым другим способом
      * Метод возвращает: 140 000
      */
-
     public int makerWithMaxProceeds(Statement stmt){
         int maxPrice = 0;
-        //todo
-        //поскольку метод JOIN мы не проходили я не смог его корректно реализовать, но логику работы алгоритма могу написать
-        //на примере объединенной базы данных Common
         ArrayList<String> listMakers = new ArrayList<>();
         ResultSet rs = null;
         try {
@@ -144,78 +147,5 @@ public class DBUtility {
             e.printStackTrace();
         }
         return maxPrice;
-    }
-
-
-    public void createListOfEquipment (Statement stmt) {
-        try {
-            stmt.execute("CREATE TABLE IF NOT EXISTS Product (maker TEXT, model INTEGER, type TEXT);");
-            stmt.execute("CREATE TABLE IF NOT EXISTS Laptop (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, model INTEGER, speed INTEGER, ram INTEGER, hd INTEGER, screen INTEGER, price INTEGER);");
-            stmt.execute("CREATE TABLE IF NOT EXISTS PC (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, model INTEGER, speed INTEGER, ram INTEGER, hd INTEGER, price INTEGER);");
-            stmt.execute("INSERT INTO Laptop(model, speed, ram, hd, screen, price)  VALUES(1360, 3600, 16, 1000, 19, 70000);");
-            stmt.execute("INSERT INTO Product(maker, model, type) VALUES('Intel', 1360, 'Laptop');");
-            stmt.execute("INSERT INTO PC(model, speed, ram, hd, price)  VALUES(2210, 3600, 8, 1000, 35000);");
-            stmt.execute("INSERT INTO Product(maker, model, type) VALUES('Intel', 2210, 'PC');");
-            stmt.execute("INSERT INTO PC(model, speed, ram, hd, price)  VALUES(2205, 2200, 4, 1000, 22000);");
-            stmt.execute("INSERT INTO Product(maker, model, type) VALUES('AMD', 2205, 'PC');");
-            stmt.execute("INSERT INTO PC(model, speed, ram, hd, price)  VALUES(2205, 2200, 4, 1000, 22000);");
-            stmt.execute("INSERT INTO Product(maker, model, type) VALUES('AMD', 2205, 'PC');");
-            stmt.execute("INSERT INTO Laptop(model, speed, ram, hd, screen, price)  VALUES(1330, 3000, 8, 1000, 17, 35000);");
-            stmt.execute("INSERT INTO Product(maker, model, type) VALUES('Dell', 1330, 'Laptop');");
-            stmt.execute("INSERT INTO Laptop(model, speed, ram, hd, screen, price)  VALUES(1305, 2000, 4, 500, 15, 15000);");
-            stmt.execute("INSERT INTO Product(maker, model, type) VALUES('HP', 1305, 'Laptop');");
-            stmt.execute("INSERT INTO Laptop(model, speed, ram, hd, screen, price)  VALUES(1305, 2000, 4, 500, 15, 15000);");
-            stmt.execute("INSERT INTO Product(maker, model, type) VALUES('HP', 1305, 'Laptop');");
-            stmt.execute("INSERT INTO Laptop(model, speed, ram, hd, screen, price)  VALUES(1305, 2000, 4, 500, 15, 15000);");
-            stmt.execute("INSERT INTO Product(maker, model, type) VALUES('HP', 1305, 'Laptop');");
-            stmt.execute("INSERT INTO PC(model, speed, ram, hd, price)  VALUES(2210, 3600, 8, 1000, 35000);");
-            stmt.execute("INSERT INTO Product(maker, model, type) VALUES('Intel', 2210, 'PC');");
-            stmt.execute("INSERT INTO Laptop(model, speed, ram, hd, screen, price)  VALUES(1305, 2000, 4, 500, 15, 15000);");
-            stmt.execute("INSERT INTO Product(maker, model, type) VALUES('HP', 1305, 'Laptop');");
-            stmt.execute("INSERT INTO Laptop(model, speed, ram, hd, screen, price)  VALUES(1305, 2000, 4, 500, 15, 5000);");
-            stmt.execute("INSERT INTO Product(maker, model, type) VALUES('HP', 1305, 'Laptop');");
-            stmt.execute("INSERT INTO Laptop(model, speed, ram, hd, screen, price)  VALUES(1290, 1200, 2, 500, 14, 10000);");
-            stmt.execute("INSERT INTO Product(maker, model, type) VALUES('Acer', 1290, 'Laptop');");
-            stmt.execute("INSERT INTO Laptop(model, speed, ram, hd, screen, price)  VALUES(1290, 1200, 2, 500, 14, 10000);");
-            stmt.execute("INSERT INTO Product(maker, model, type) VALUES('Acer', 1290, 'Laptop');");
-            stmt.execute("INSERT INTO Laptop(model, speed, ram, hd, screen, price)  VALUES(1290, 1200, 2, 500, 14, 10000);");
-            stmt.execute("INSERT INTO Product(maker, model, type) VALUES('Acer', 1290, 'Laptop');");
-            stmt.execute("INSERT INTO PC(model, speed, ram, hd, price)  VALUES(2200, 1700, 2, 500, 12000);");
-            stmt.execute("INSERT INTO PC(model, speed, ram, hd, price)  VALUES(2200, 1700, 2, 500, 12000);");
-            stmt.execute("INSERT INTO PC(model, speed, ram, hd, price)  VALUES(2200, 1700, 2, 500, 12000);");
-            stmt.execute("INSERT INTO PC(model, speed, ram, hd, price)  VALUES(2200, 1700, 2, 500, 12000);");
-            stmt.execute("INSERT INTO Product(maker, model, type) VALUES('NEC', 2200, 'PC');");
-            stmt.execute("INSERT INTO Product(maker, model, type) VALUES('NEC', 2200, 'PC');");
-            stmt.execute("INSERT INTO Product(maker, model, type) VALUES('NEC', 2200, 'PC');");
-            stmt.execute("INSERT INTO Product(maker, model, type) VALUES('NEC', 2200, 'PC');");
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void createCommonBase (Statement stmt){
-        try {
-            stmt.execute("CREATE TABLE IF NOT EXISTS Common(id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, maker TEXT, model INTEGER, type TEXT, speed INTEGER, ram INTEGER, hd INTEGER, screen INTEGER, price INTEGER);");
-            stmt.execute("INSERT INTO Common(maker, model, type, speed, ram, hd, screen, price) VALUES('Intel', 1360, 'Laptop', 3600, 16, 1000, 19, 70000);");
-            stmt.execute("INSERT INTO Common(maker, model, type, speed, ram, hd, screen, price) VALUES('Intel', 2210, 'PC', 3600, 8, 1000, 20, 35000);");
-            stmt.execute("INSERT INTO Common(maker, model, type, speed, ram, hd, screen, price) VALUES('AMD', 2205, 'PC', 2200, 4, 1000, 20, 22000);");
-            stmt.execute("INSERT INTO Common(maker, model, type, speed, ram, hd, screen, price) VALUES('AMD', 2205, 'PC', 2200, 4, 1000, 20, 22000);");
-            stmt.execute("INSERT INTO Common(maker, model, type, speed, ram, hd, screen, price) VALUES('Dell', 1330, 'Laptop', 3000, 8, 1000, 17, 35000);");
-            stmt.execute("INSERT INTO Common(maker, model, type, speed, ram, hd, screen, price) VALUES('HP', 1305, 'Laptop', 2000, 4, 500, 15, 15000);");
-            stmt.execute("INSERT INTO Common(maker, model, type, speed, ram, hd, screen, price) VALUES('HP', 1305, 'Laptop', 2000, 4, 500, 15, 15000);");
-            stmt.execute("INSERT INTO Common(maker, model, type, speed, ram, hd, screen, price) VALUES('HP', 1305, 'Laptop', 2000, 4, 500, 15, 15000);");
-            stmt.execute("INSERT INTO Common(maker, model, type, speed, ram, hd, screen, price) VALUES('Intel', 2210, 'PC', 3600, 8, 1000, 20, 35000);");
-            stmt.execute("INSERT INTO Common(maker, model, type, speed, ram, hd, screen, price) VALUES('HP', 1305, 'Laptop', 2000, 4, 500, 15, 150000);");
-            stmt.execute("INSERT INTO Common(maker, model, type, speed, ram, hd, screen, price) VALUES('HP', 1305, 'Laptop', 2000, 4, 500, 15, 5000);");
-            stmt.execute("INSERT INTO Common(maker, model, type, speed, ram, hd, screen, price) VALUES('Acer', 1290, 'Laptop', 1200, 2, 500, 14, 10000);");
-            stmt.execute("INSERT INTO Common(maker, model, type, speed, ram, hd, screen, price) VALUES('Acer', 1290, 'Laptop', 1200, 2, 500, 14, 10000);");
-            stmt.execute("INSERT INTO Common(maker, model, type, speed, ram, hd, screen, price) VALUES('Acer', 1290, 'Laptop', 1200, 2, 500, 14, 10000);");
-            stmt.execute("INSERT INTO Common(maker, model, type, speed, ram, hd, screen, price) VALUES('NEC', 2200, 'PC', 1700, 2, 500, 20, 12000);");
-            stmt.execute("INSERT INTO Common(maker, model, type, speed, ram, hd, screen, price) VALUES('NEC', 2200, 'PC', 1700, 2, 500, 20, 12000);");
-            stmt.execute("INSERT INTO Common(maker, model, type, speed, ram, hd, screen, price) VALUES('NEC', 2200, 'PC', 1700, 2, 500, 20, 12000);");
-            stmt.execute("INSERT INTO Common(maker, model, type, speed, ram, hd, screen, price) VALUES('NEC', 2200, 'PC', 1700, 2, 500, 20, 12000);");
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
     }
 }
