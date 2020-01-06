66955ebfd3f1978d39d9b79017ff43d23166d489
master-kot <master-kot@mail.ru>
2020-01-07 01:13:27 +0300

    homework for lesson 4 in progress

--- lesson4/src/main/java/TaskRunner.java
+++ lesson4/src/main/java/TaskRunner.java
@@ -2,13 +2,36 @@ import java.util.LinkedList;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class TaskRunner implements Runnable {
-    
-	Object mutex;	//монитор синхронизации
+    public static void main(String[] args) {
+        ThreadTask task = new ThreadTask();
+        Object mon = new Object();
+        for (int j = 0; j < 3; j++) {
+            TaskRunner [] runners = new TaskRunner[3];
+            Thread [] threads = new Thread[3];
+            for (int i = 0; i < 3; i++) {
+                runners[i] = new TaskRunner(mon, String.valueOf(i+1), i);
+                threads[i] = new Thread(runners[i]);
+            }
+            task.multiThreadRecord(threads);
+            try {
+                Thread.sleep(1000);
+                int cnt = 0;
+                int [] arr = new int[]{1,2,3};
+                System.out.println(TaskRunner.list);
+                for (int i = 0; i < 9; i++) {
+                    System.out.println(String.valueOf(arr[cnt]) + ":" + TaskRunner.list.get(i) + ":" + String.valueOf(arr[cnt]).equals(TaskRunner.list.get(i)));
+                    cnt = (cnt + 1) % 3;
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace(); } } }
+
+	final Object mutex;	//монитор синхронизации
     String message;	//сообщение которое поток будет добавлять в список
     volatile int cnt;
     volatile int inc;
-    static volatile int iter = 0;
-	/**
+    volatile int iter = 0;
+
+    /**
      * этот список будет проверяться на предмет синхронизации данных,
      * а именно на соответствие: [1, 2, 3, 1, 2, 3, 1, 2, 3....]
 	 */
@@ -23,13 +46,32 @@ public class TaskRunner implements Runnable {
     
 	//TODO: 26.12.2019
 	/**
-	 * реализовать логику записи сообщений в list одновременно из 
+	 * реализовать запись сообщений в list одновременно из
 	 * трех запущенных потоков с соблюдением синхронизации записываемых данных
 	 */
     @Override
     public void run() {
+
+
         while (true) {
-            list.add(message);
+            synchronized (mutex) {
+                if (cnt == 0 & iter == 0) {
+                   //mutex.notifyAll();
+                   list.add(message);
+                   iter = Integer.parseInt(message);
+                   //mutex.wait();
+               } else if (cnt == 1 & iter == 1) {
+                   //mutex.notifyAll();
+                   list.add(message);
+                   iter = Integer.parseInt(message);
+                   //mutex.wait();
+               } else if (cnt == 2 & iter == 2) {
+                   //mutex.notifyAll();
+                   list.add(message);
+                   iter = 0;
+                   //mutex.wait();
+               }
+            }
         }
     }
 }
