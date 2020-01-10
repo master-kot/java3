d30480a25f90af34d2e3aa5ab7eb13dfbb1a36e8
master-kot <master-kot@mail.ru>
2020-01-10 23:25:34 +0300

    homework for lesson 4 in finished

--- lesson4/src/main/java/TaskRunner.java
+++ lesson4/src/main/java/TaskRunner.java
@@ -1,32 +1,34 @@
 import java.util.LinkedList;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class TaskRunner implements Runnable {
-	final Object mutex;	        //монитор синхронизации
-    String message;	            //сообщение которое поток будет добавлять в список
+
+    final Object mutex;	        //монитор синхронизации
+    String message;	            //сообщение которое каждый поток будет добавлять в список
     volatile int cnt;
     static volatile int inc;    //переменная позволяющая менять очередность выполнения потоками
 
     /**
      * этот список будет проверяться на предмет синхронизации данных,
      * а именно на соответствие: [1, 2, 3, 1, 2, 3, 1, 2, 3....]
-	 */
+     */
     static volatile LinkedList<String> list = new LinkedList<>();
-	
-	public TaskRunner(Object mutex, String msg, int cnt) {
+
+    public TaskRunner(Object mutex, String msg, int cnt) {
         this.mutex = mutex;
         message = msg; //Каждый поток должен писать в список только свое сообщение
         this.cnt = cnt;
         inc = 0;
-    } 
-    
-	//TODO: 26.12.2019
-	/**
-	 * реализовать запись сообщений в list одновременно из
-	 * трех запущенных потоков с соблюдением синхронизации записываемых данных
-	 */
+    }
+
+    //TODO: 26.12.2019
+    /**
+     * реализовать запись сообщений в list одновременно из трех
+     * запущенных потоков с соблюдением синхронизации и порядка записываемых данных
+     */
     @Override
     public void run() {
-        while (true) {
+        for (int i = 0; i < 9; i++) {
             synchronized (mutex) {
                 try {
                     if (cnt == 0) {
