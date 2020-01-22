c1aa619ba613b7c0912a68f4970a37ffb4bcaf6b
master-kot <master-kot@mail.ru>
2020-01-22 20:19:39 +0300

    homework for the lesson 7 was modified (classes Tests and TestInvoker were changed)

--- lesson7/src/main/java/annotation/Tests.java
+++ lesson7/src/main/java/annotation/Tests.java
@@ -7,23 +7,24 @@ public class Tests {
     @Before
     public void starter(){
         System.out.println("Test starts!");
-        util = new Utility(25, 13);
+        util = new Utility(26, 13);
     }
 
     @Test(priority = 1)
     public void sumTest(){
         System.out.println("Sum test");
-        if (util.sum() == 38){
+        if (util.sum() == 39){
             System.out.println("OK");
         } else {
             System.out.println("WRONG");
         }
     }
 
+    // TODO: 23/01/2020
     @Test(priority = 2)
     public void subTest(){
         System.out.println("Sub test");
-        if (util.sum() == 12){
+        if (util.sum() == 13){
             System.out.println("OK");
         } else {
             System.out.println("WRONG");
@@ -33,19 +34,34 @@ public class Tests {
     // TODO: 23/01/2020
     @Test(priority = 3)
     public void mulTest(){
-
+        System.out.println("Multiply test");
+        if (util.mul() == 338){
+            System.out.println("OK");
+        } else {
+            System.out.println("WRONG");
+        }
     }
 
     // TODO: 23/01/2020
     @Test(priority = 4)
     public void divTest(){
-
+        System.out.println("Divide test");
+        if (util.div() == 2){
+            System.out.println("OK");
+        } else {
+            System.out.println("WRONG");
+        }
     }
 
     // TODO: 23/01/2020
     @Test(priority = 5)
     public void modTest(){
-
+        System.out.println("Mod test");
+        if (util.div() == 0){
+            System.out.println("OK");
+        } else {
+            System.out.println("WRONG");
+        }
     }
 
     @After
--- lesson7/src/main/java/annotation/TestsInvoker.java
+++ lesson7/src/main/java/annotation/TestsInvoker.java
@@ -5,13 +5,60 @@ import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.Comparator;
 
-public class TestsInvoker {
-    // TODO: 23/01/2020
-    public void invokeTests(Class c) throws InvocationTargetException, IllegalAccessException {
+/**
+ * Создать класс, который может выполнять «тесты».
+ * В качестве тестов выступают классы с наборами методов, снабженных аннотациями @Test.
+ * Класс, запускающий тесты, должен иметь статический метод start(Class testClass),
+ * которому в качестве аргумента передается объект типа Class.
+ * Из «класса-теста» вначале должен быть запущен метод с аннотацией @BeforeSuite,
+ * если он присутствует. Далее запускаются методы с аннотациями @Test,
+ * а по завершении всех тестов – метод с аннотацией @AfterSuite.
+ * К каждому тесту необходимо добавить приоритеты (int-числа от 1 до 10),
+ * в соответствии с которыми будет выбираться порядок их выполнения.
+ * Если приоритет одинаковый, то порядок не имеет значения.
+ * Методы с аннотациями @BeforeSuite и @AfterSuite должны присутствовать в единственном экземпляре.
+ * Если это не так – необходимо бросить RuntimeException при запуске «тестирования».
+ */
 
+public class TestsInvoker {
+
+    // TODO: 23/01/2020
+    public static void invokeTests(Class c) throws InvocationTargetException, InstantiationException, IllegalAccessException {
+        //Method methodBefore = null;
+        //Method methodAfter = null;
+        //Method[] methodsTest;
+
+        int numberOfMethodBefore = 0;
+        int numberOfMethodAfter = 0;
+        boolean isThereMethodBefore = false;
+        boolean isThereMethodAfter = false;
+
+        Method[] listOfMethods = c.getMethods();
+
+        for (int i = 0; i < listOfMethods.length; i++) {
+            if (listOfMethods[i].getAnnotation(Before.class) != null) {
+                if (!isThereMethodBefore) {
+                    //methodBefore = listOfMethods[i];
+                    numberOfMethodBefore = i;
+                    isThereMethodBefore = false;
+                } else throw new RuntimeException("Before must be only one");
+            } else if (listOfMethods[i].getAnnotation(Test.class) != null) {
+            } else if (listOfMethods[i].getAnnotation(After.class) != null) {
+                if (!isThereMethodAfter) {
+                    //methodAfter = listOfMethods[i];
+                    numberOfMethodAfter = i;
+                } else throw new RuntimeException("After must be only one");
+            }
+        }
+
+        Tests myTests = (Tests)c.newInstance();
+        //methodBefore.invoke(myTests);
+        //methodAfter.invoke(myTests);
+        listOfMethods[numberOfMethodBefore].invoke(myTests);
+        listOfMethods[numberOfMethodAfter].invoke(myTests);
     }
 
-    public static void main(String[] args) throws InvocationTargetException, IllegalAccessException {
+    public static void main(String[] args) throws InvocationTargetException, IllegalAccessException, InstantiationException {
         Class clazz = Tests.class;
         new TestsInvoker().invokeTests(clazz);
     }
